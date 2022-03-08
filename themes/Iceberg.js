6ec070de778d53468137f2801a99e32a93b20e4c
comgy <gentoo.gk@gmail.com>
2022-03-08 22:15:56 +0900

    feat: add iceberg theme

---
+++ themes/Iceberg.js
@@ -0,0 +1,41 @@
+// Copyright (c) 2014 cocopon <cocopon@me.com>
+// Licensed under MIT (https://github.com/cocopon/iceberg.vim/blob/master/LICENSE.txt)
+
+(black = "#1e2132"),
+  (red = "#e27878"),
+  (green = "#b4be82"),
+  (yellow = "#e2a478"),
+  (blue = "#84a0c6"),
+  (magenta = "#a093c7"),
+  (cyan = "#89b8c2"),
+  (white = "#c6c8d1"),
+  (lightBlack = "#6b7089"),
+  (lightRed = "#e98989"),
+  (lightGreen = "#c0ca8e"),
+  (lightYellow = "#e9b189"),
+  (lightBlue = "#91acd1"),
+  (lightMagenta = "#ada0d3"),
+  (lightCyan = "#95c4ce"),
+  (lightWhite = "#d2d4de"),
+  t.prefs_.set("color-palette-overrides", [
+    black,
+    red,
+    green,
+    yellow,
+    blue,
+    magenta,
+    cyan,
+    white,
+    lightBlack,
+    lightRed,
+    lightGreen,
+    lightYellow,
+    lightBlue,
+    lightMagenta,
+    lightCyan,
+    lightWhite,
+  ]);
+
+t.prefs_.set("cursor-color", "rgba(198, 200, 209, 0.5)");
+t.prefs_.set("foreground-color", "#c6c8d1");
+t.prefs_.set("background-color", "#161821");
